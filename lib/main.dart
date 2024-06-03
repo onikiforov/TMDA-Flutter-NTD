c42d789ddd990acc8bb16d62215510a43a263434
Oleg Nikiforov <ddr3ams@gmail.com>
2024-06-03 21:50:58 +0200

    add support for proxy usage; update dependencies; bump Java version; add network security config

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,21 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:tmda/app.dart';
 import 'package:tmda/bloc_observer.dart';
 import 'package:tmda/injection_container.dart';
+import 'package:native_flutter_proxy/native_proxy_reader.dart';
+import 'package:native_flutter_proxy/custom_proxy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  String? host;
+  int? port;
+  ProxySetting settings = await NativeProxyReader.proxySetting;
+  host = settings.host;
+  port = settings.port;
+  if (host != null && port != null) {
+    final proxy = CustomProxy(ipAddress: host, port: port);
+    proxy.enable();
+  }
   configureDependencies(); 
   Bloc.observer = MyBlocObserver();
   runApp(const TmdaApp());
